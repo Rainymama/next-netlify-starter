918490ba56482f630279ff9cedd8b5ecb8f1faa6
Rainymama <100744971+Rainymama@users.noreply.github.com>
2022-08-26 11:31:20 -0700

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
       <main>
         <Header title="Commonplaces" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          contingency, dependence, responsibility
         </p>
       </main>
 
